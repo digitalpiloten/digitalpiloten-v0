2b474cb13ead57901f00791c9226812abcecf9f3
MagicMarvMan <marvin.kai@gmx.de>
2019-12-18 21:38:18 +0100

    Add component for page

--- src/Page.jsx
+++ src/Page.jsx
@@ -3,46 +3,51 @@ import { Query } from 'react-contentful';
 
 import ReactMarkdown from 'react-markdown';
  
-const Page = (props) => (
-  <Query
-    contentType="page"
-    query={{
-      'fields.slug[in]': `/${props.match.slug || ''}`,
-    }}
-  >
-    {({data, error, fetched, loading}) => {
-      if (loading || !fetched) {
-        return null;
-      }
- 
-      if (error) {
-        console.error(error);
-        return null;
-      }
- 
-      if (!data) {
-        return <p>404 - Page does not exist.</p>;
-      }
+class Page extends React.Component {
 
-      if(!data.items) {
-        return <p>404 - Page does not exist.</p>;
-      }
+  render() {
+    return (
+      <Query
+        contentType="page"
+        query={{
+          'fields.slug[in]': `/${this.props.match.params.slug || ''}`,
+        }}
+      >
+        {({data, error, fetched, loading}) => {
+          if (loading || !fetched) {
+            return null;
+          }
+    
+          if (error) {
+            console.error(error);
+            return null;
+          }
+    
+          if (!data) {
+            return <p>404 - Page does not exist.</p>;
+          }
 
-      if(data.items.length < 1) {
-        return <p>404 - Page does not exist.</p>;
-      }
- 
-      // See the Contentful query response
-      //console.debug(data);
- 
-      // Process and pass in the loaded `data` necessary for your page or child components
-      return (
-        <div>
-          <ReactMarkdown source={data.items[0].fields.content} />
-        </div>
-      );
-    }}
-  </Query>
-);
+          if(!data.items) {
+            return <p>404 - Page does not exist.</p>;
+          }
+
+          if(data.items.length < 1) {
+            return <p>404 - Page does not exist.</p>;
+          }
+    
+          // See the Contentful query response
+          //console.debug(data);
+    
+          // Process and pass in the loaded `data` necessary for your page or child components
+          return (
+            <div>
+              <ReactMarkdown source={data.items[0].fields.content} />
+            </div>
+          );
+        }}
+      </Query>
+    )
+  }
+}
  
 export default Page;
