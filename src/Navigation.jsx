c4609916334778570a8bafc19c1134f3f4ca6221
MagicMarvMan <marvin.kai@gmx.de>
2019-12-21 11:37:14 +0100

    Use tailwind navigation

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
 
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import {
+    Link,
+} from "react-router-dom"
 
-import { LinkContainer } from 'react-router-bootstrap'
+import './css/tailwind.css';
 
 class Navigation extends React.Component {
     render() {
         return (
-            <Navbar bg="light" expand="lg">
-                <LinkContainer to="/">
-                    <Navbar.Brand>DigitalPiloten e. V.</Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <LinkContainer to="/">
-                            <Nav.Link>Home</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/about">
-                            <Nav.Link>Über uns</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/contact">
-                            <Nav.Link>Kontakt</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+                <div className="flex items-center flex-shrink-0 text-white mr-6">
+                    <span className="font-semibold text-xl tracking-tight">DigitalPiloten e. V.</span>
+                </div>
+                <div className="block lg:hidden">
+                    <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                        <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                    </button>
+                </div>
+                <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                    <div class="text-sm lg:flex-grow">
+                        <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                            Home
+                        </Link>
+                        <Link to="/about" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                            Examples
+                        </Link>
+                        <Link to="/contact" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                            Kontakt
+                        </Link>
+                        </div>
+                        <div>
+                        <Link to="/donate" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Spenden</Link>
+                    </div>
+                </div>
+            </nav>
         )
     }
 }
