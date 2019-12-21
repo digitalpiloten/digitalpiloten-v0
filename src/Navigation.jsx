bb3d3f54d261e41b1db0d2a3ede04ced3a5d81a6
MagicMarvMan <marvin.kai@gmx.de>
2019-12-21 11:39:36 +0100

    Change  navbar content

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -24,14 +24,14 @@ class Navigation extends React.Component {
                             Home
                         </Link>
                         <Link to="/about" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                            Examples
+                            Über uns
                         </Link>
                         <Link to="/contact" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                             Kontakt
                         </Link>
                         </div>
                         <div>
-                        <Link to="/donate" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Spenden</Link>
+                        <a href="/donate" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Veranstaltungen</a>
                     </div>
                 </div>
             </nav>
