480526e450c1cf1cf2c4608aaba4e3c0a42b004e
MagicMarvMan <marvin.kai@gmx.de>
2019-12-22 21:53:16 +0100

    Add links to legal documents to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Footer, Content } from 'react-bulma-components';
 
+import { Link } from "react-router-dom";
+
 //import { LinkContainer } from 'react-router-bootstrap'
 
 class Navigation extends React.Component {
@@ -12,6 +14,16 @@ class Navigation extends React.Component {
                     <center>
                         <p>
                             Copyright &copy; 2019 DigitalPiloten e. V. Alle Rechte vorbehalten.
+                        </p>
+                        <br />
+                        <p>
+                            <Link to="/impressum">
+                                Impressum
+                            </Link>
+                            &nbsp;|&nbsp;
+                            <Link to="/datenschutzerklaerung">
+                                Datenschutzerklärung
+                            </Link>
                         </p>
                     </center>
                 </Content>
